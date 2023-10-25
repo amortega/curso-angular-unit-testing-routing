129e64d1157ab9c4b5ec2f03ee58948c11d3c451
Andrés Ortega <arguello.25018@gmail.com>
2023-10-25 09:43:06 -0300

    all tests

--- src/app/components/others/others.component.ts
+++ src/app/components/others/others.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './../../models/product.model';
+import { ProductsService } from './../../services/product.service';
 
 @Component({
   selector: 'app-others',
   templateUrl: './others.component.html',
   styleUrls: ['./others.component.scss']
 })
-export class OthersComponent{
+export class OthersComponent implements OnInit {
 
   color = 'yellow';
   text = 'Un texto';
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private productService: ProductsService
+  ) { }
+
+  ngOnInit() {
+    this.productService.getAll()
+    .subscribe(data => {
+      this.products = data;
+    })
+  }
 
 }
